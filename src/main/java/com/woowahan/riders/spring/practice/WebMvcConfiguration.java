e7404734232d1b9ec5e5691830d7b7f952c9e5d5
leeJaeil <leejaeil@woowahan.com>
2016-03-16 16:27:59 +0900

    spring mvc hiddenHttpMethodFilter,httpPutFormContentFilter 추가.

--- src/main/java/com/woowahan/riders/spring/practice/WebMvcConfiguration.java
+++ src/main/java/com/woowahan/riders/spring/practice/WebMvcConfiguration.java
@@ -1,7 +1,14 @@
 package com.woowahan.riders.spring.practice;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.web.OrderedHiddenHttpMethodFilter;
+import org.springframework.boot.context.web.OrderedHttpPutFormContentFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,6 +20,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan(basePackageClasses = {WebMvcConfiguration.class})
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    @ConditionalOnMissingBean(HiddenHttpMethodFilter.class)
+    public OrderedHiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new OrderedHiddenHttpMethodFilter();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(HttpPutFormContentFilter.class)
+    public OrderedHttpPutFormContentFilter httpPutFormContentFilter() {
+        return new OrderedHttpPutFormContentFilter();
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/home");
